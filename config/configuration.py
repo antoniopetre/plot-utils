c5ef9d2189a982039009ffffef712e2665eef671
brusale <brusale11@gmail.com>
2024-02-07 16:51:18 +0100

    add configuration and style

---
+++ config/configuration.py
@@ -0,0 +1,8 @@
+configuration = {
+    "FilesDirectory" : "",
+    "doPFComparison" : True,
+    "cmsText" : "Simulation",
+    "Annotate" : "SinglePhoton @200PU\n" r"E$_{gun}\in$ [1, 600] GeV",
+    "SaveDirectory" : "",
+    "DebugMode" : True 
+}
---
+++ config/style.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import mplhep as hep
+
+plt.style.use([hep.style.ROOT, hep.style.firamath])
+mpl.rcParams['figure.figsize'] = [10, 10]
+mpl.rcParams['font.size'] = 20
+mpl.rcParams['xtick.labelsize'] = 20
+mpl.rcParams['ytick.labelsize'] = 20
+mpl.rcParams['axes.labelsize'] = 30
