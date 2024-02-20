7c2e91718508c60acda0f4193e3f8a2d67743dee
brusale <brusale11@gmail.com>
2024-02-20 14:27:46 +0100

    remove perLayerPlots and split cumulative ones

--- plotting/EfficiencyPlots.py
+++ plotting/EfficiencyPlots.py
@@ -27,19 +27,16 @@ class EfficiencyPlots:
 
         self.makePlot(cumulative=False)
         self.makePlot(cumulative=True)
-        for layer in range(0, 7):
-            self.makePlot(cumulative=False, layer=layer)
 
-    def makePlot(self, cumulative, layer=None):
-        cumulative = int(cumulative)
+    def makePlot(self, cumulative):
         fig, axs = plt.subplots(1, 3, figsize=(30, 15))
         for cp_dict, k in zip(self.cp_dict_arr, range(0, len(self.cp_dict_arr))):
-            if (layer == None):
-                eta_eff, phi_eff, energy_eff = count(cp_dict, self.lc_dict_arr[k])
+            if (cumulative):
+                eta_eff, phi_eff, energy_eff = countCumulative(cp_dict)
             else:
-                eta_eff, phi_eff, energy_eff = countPerLayer(cp_dict, self.lc_dict_arr[k], layer)
-              
-            num_eta_counts, _ = np.histogram(eta_eff[0][cumulative], bins=self.eta_bins)            
+                eta_eff, phi_eff, energy_eff = count(cp_dict)
+            
+            num_eta_counts, _ = np.histogram(eta_eff[0], bins=self.eta_bins)            
             denom_eta_counts, _ = np.histogram(eta_eff[1], bins=self.eta_bins)            
            
             eta_ratio, eta_ratio_error = makeRatio(num_eta_counts, denom_eta_counts)
@@ -51,7 +48,7 @@ class EfficiencyPlots:
             axs[0].set_xlabel(r"$\eta$")
             axs[0].set_xlim(-1.2, 1.2) 
        
-            num_phi_counts, _ = np.histogram(phi_eff[0][cumulative], bins=self.phi_bins)            
+            num_phi_counts, _ = np.histogram(phi_eff[0], bins=self.phi_bins)            
             denom_phi_counts, _ = np.histogram(phi_eff[1], bins=self.phi_bins)            
                                                                                             
             phi_ratio, phi_ratio_error = makeRatio(num_phi_counts, denom_phi_counts)
@@ -63,7 +60,7 @@ class EfficiencyPlots:
             axs[1].set_xlabel(r"$\phi$")
             axs[1].set_xlim(-np.pi, np.pi) 
 
-            num_energy_counts, _ = np.histogram(energy_eff[0][cumulative], bins=self.energy_bins)            
+            num_energy_counts, _ = np.histogram(energy_eff[0], bins=self.energy_bins)            
             denom_energy_counts, _ = np.histogram(energy_eff[1], bins=self.energy_bins)            
                                                                                             
             energy_ratio, energy_ratio_error = makeRatio(num_energy_counts, denom_energy_counts)
@@ -86,34 +83,22 @@ class EfficiencyPlots:
                 else:
                     ax.annotate(text=self.annotate, xy=(ax.get_xlim()[1]*0.2, 0.4), fontsize=20)
 
-        if (layer == None):
-            if (cumulative) :
-                if (self.save != None and len(self.c) == 2):
-                    fig.savefig(self.save+"efficiency_comparison_cumulative.pdf")
-                elif (self.save != None):
-                    fig.savefig(self.save+"efficiency_cumulative.pdf")
-            else:
-                if (self.save != None and len(self.c) == 2):
-                    fig.savefig(self.save+"efficiency_comparison.pdf")
-                elif (self.save != None):
-                    fig.savefig(self.save+"efficiency.pdf")
-    
+        if (cumulative) :
+            if (self.save != None and len(self.c) == 2):
+                fig.savefig(self.save+"efficiency_comparison_cumulative.pdf")
+            elif (self.save != None):
+                fig.savefig(self.save+"efficiency_cumulative.pdf")
         else:
-            if (cumulative) :
-                if (self.save != None and len(self.c) == 2):
-                    fig.savefig(self.save+"efficiency_comparison_cumulative_layer%s.pdf" % layer)
-                elif (self.save != None):
-                    fig.savefig(self.save+"efficiency_cumulative_layer%s.pdf" % layer)
-            else:
-                if (self.save != None and len(self.c) == 2):
-                    fig.savefig(self.save+"efficiency_comparison_layer%s.pdf" % layer)
-                elif (self.save != None):
-                    fig.savefig(self.save+"efficiency_layer%s.pdf" % layer)
+            if (self.save != None and len(self.c) == 2):
+                fig.savefig(self.save+"efficiency_comparison.pdf")
+            elif (self.save != None):
+                fig.savefig(self.save+"efficiency.pdf")
+    
 
         fig.clf()
 
 
-def count(cp_dict, lc_dict):
+def count(cp_dict):
     num_eta, denom_eta = [], []
     num_phi, denom_phi = [], []
     num_energy, denom_energy = [], []
@@ -122,68 +107,46 @@ def count(cp_dict, lc_dict):
     for event in range(0, len(cp_dict)):
         event = str(event+1)
         for cp in range(0, len(cp_dict[event]['CaloParticleEnergy'])):
-            total_shared_energy = 0
-            if (len(cp_dict[event]['CP2LCscore'][cp]) == 0) : continue
+            is_efficient = False
+            if (len(cp_dict[event]['CP2LCscore'][cp]) == 0) or is_efficient : continue
             denom_eta.append(cp_dict[event]['CaloParticleEta'][cp])
             denom_phi.append(cp_dict[event]['CaloParticlePhi'][cp])
-            #denom_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
             denom_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
             if len(cp_dict[event]['SharedEnergy'][cp]) == 0 : continue
-            for energy in cp_dict[event]['SharedEnergy'][cp]:
-                total_shared_energy += energy / cp_dict[event]['CaloParticleEnergy'][cp]
             max_shared_energy = max(cp_dict[event]['SharedEnergy'][cp])
             max_shared_energy /= cp_dict[event]['CaloParticleEnergy'][cp]
             if max_shared_energy > 0.7:
                 num_eta.append(cp_dict[event]['CaloParticleEta'][cp])         
                 num_phi.append(cp_dict[event]['CaloParticlePhi'][cp])
                 num_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
-            if total_shared_energy > 0.7:
-                cumulative_eta.append(cp_dict[event]['CaloParticleEta'][cp])
-                cumulative_phi.append(cp_dict[event]['CaloParticlePhi'][cp])
-                cumulative_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
 
-    return [[num_eta,cumulative_eta], denom_eta], [[num_phi, cumulative_phi], denom_phi], [[num_energy, cumulative_energy], denom_energy]
+    return [num_eta, denom_eta], [num_phi, denom_phi], [num_energy, denom_energy]
 
-def countPerLayer(cp_dict, lc_dict, layer):
-    num_eta, denom_eta = [], []                                                                                                            
+
+def countCumulative(cp_dict):
+    num_eta, denom_eta = [], []
     num_phi, denom_phi = [], []
     num_energy, denom_energy = [], []
-                                                                                                                                       
-    cumulative_eta, cumulative_phi, cumulative_energy = [], [], []
+
     for event in range(0, len(cp_dict)):
-         event = str(event+1)
-         for cp in range(0, len(cp_dict[event]['CaloParticleEnergy'])):
-             total_shared_energy = 0
-             if (len(cp_dict[event]['CP2LCscore'][cp]) == 0) : continue
-             denom_eta.append(cp_dict[event]['CaloParticleEta'][cp])
-             denom_phi.append(cp_dict[event]['CaloParticlePhi'][cp])
-             denom_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
-             if len(cp_dict[event]['SharedEnergy'][cp]) == 0 : continue
-             for energy in cp_dict[event]['SharedEnergy'][cp]:
-                 total_shared_energy += energy / cp_dict[event]['CaloParticleEnergy'][cp]
-             max_shared_energy = max(cp_dict[event]['SharedEnergy'][cp])
-             arg_max_shared_energy = np.argmax(cp_dict[event]['SharedEnergy'][cp])
-             if (lc_dict[event]['LayerClustersLayer'][arg_max_shared_energy] != layer): continue
-             max_shared_energy /= cp_dict[event]['CaloParticleEnergy'][cp]
-             if max_shared_energy > 0.7:
-                 num_eta.append(cp_dict[event]['CaloParticleEta'][cp])         
-                 num_phi.append(cp_dict[event]['CaloParticlePhi'][cp])
-                 num_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
-             if total_shared_energy > 0.7:
-                 cumulative_eta.append(cp_dict[event]['CaloParticleEta'][cp])
-                 cumulative_phi.append(cp_dict[event]['CaloParticlePhi'][cp])
-                 cumulative_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
-                                                                                                                                           
-    return [[num_eta,cumulative_eta], denom_eta], [[num_phi, cumulative_phi], denom_phi], [[num_energy, cumulative_energy], denom_energy]
-
-
-
-
-
-
-
-
-
-
+        event = str(event+1)
+        for cp in range(0, len(cp_dict[event]['CaloParticleEnergy'])):
+            total_shared_energy = 0 
+            is_efficient = False
+            if (len(cp_dict[event]['CP2LCscore'][cp]) == 0) or is_efficient : continue
+            denom_eta.append(cp_dict[event]['CaloParticleEta'][cp])
+            denom_phi.append(cp_dict[event]['CaloParticlePhi'][cp])
+            denom_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
+            if len(cp_dict[event]['SharedEnergy'][cp]) == 0 : continue
+            for energy in cp_dict[event]['SharedEnergy'][cp]:
+                total_shared_energy += energy
+            total_shared_energy /= cp_dict[event]['CaloParticleEnergy'][cp]
+            if total_shared_energy > 0.7:
+                is_efficient = True
+                num_eta.append(cp_dict[event]['CaloParticleEta'][cp])
+                num_phi.append(cp_dict[event]['CaloParticlePhi'][cp])
+                num_energy.append(cp_dict[event]['CaloParticleEnergy'][cp])
+            
+    return [num_eta, denom_eta], [num_phi, denom_phi], [num_energy, denom_energy]
 
  
